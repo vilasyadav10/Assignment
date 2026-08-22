7976db462fad52be55e83deeb754d29d392a9f08
vilasyadav10 <vilasy2003@gmail.com>
2026-08-22 20:00:36 +0530

    xnor_gate

---
+++ assignment/xnor_gate/xnor_gate.sv
@@ -0,0 +1,7 @@
+module xnor_gate(
+  input a,b,
+  output y
+);
+     assign y = !(a^b);
+
+endmodule 
---
+++ assignment/xnor_gate/xnor_gate_tb.sv
@@ -0,0 +1,19 @@
+module xnor_gate_tb;
+ reg a,b;
+ wire y;
+  xnor_gate dut(.a(a), .b(b), .y(y));
+
+  initial begin 
+       a=0; b=0;
+      #10 a=0; b=1;
+      #10 a=1; b=0;
+      #10 a=1; b=1;
+      #10; 
+      $finish;
+      end
+ 
+      initial begin
+      $monitor($time,"a=%b,b=%b,y=%b",a,b,y);
+      end
+ 
+endmodule
